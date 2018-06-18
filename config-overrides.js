77ad839464b1eaa3dbac312447ea8adefbd55a8e
Zhu Hanfei <hanfei@huawen.com>
2018-06-18 18:49:36 +0800

    removed react-app

--- config-overrides.js
+++ config-overrides.js
@@ -1,20 +1,20 @@
-// const rewireMobX = require('react-app-rewire-mobx');
-const {injectBabelPlugin} = require('react-app-rewired');
-// const rewirePreact = require('react-app-rewire-preact');
-
-/* config-overrides.js */
-
-module.exports = function override(config, env) {
-
-  config = injectBabelPlugin('transform-class-properties/babel',config);
-
-  // if (env === "production") {
-  // console.log("⚡ Production build with Preact");
-  // config = rewirePreact(config, env);
-  // }
-  //
-  // // use the MobX rewire
-  // config = rewireMobX(config,env);
-
-  return config;
-}
+// // const rewireMobX = require('react-app-rewire-mobx');
+// const {injectBabelPlugin} = require('react-app-rewired');
+// // const rewirePreact = require('react-app-rewire-preact');
+//
+// /* config-overrides.js */
+//
+// module.exports = function override(config, env) {
+//
+//   config = injectBabelPlugin('transform-class-properties/babel',config);
+//
+//   // if (env === "production") {
+//   // console.log("⚡ Production build with Preact");
+//   // config = rewirePreact(config, env);
+//   // }
+//   //
+//   // // use the MobX rewire
+//   // config = rewireMobX(config,env);
+//
+//   return config;
+// }
